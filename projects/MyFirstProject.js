5fd6fd919253f1ea0d10e56b299a03cb72b58476
WilliamShelbyDeMoss <wshelbydemoss@gmail.com>
2017-11-22 19:40:13 +0000

    A basic website

---
+++ projects/MyFirstProject.js
@@ -0,0 +1,12 @@
+var correct = true;
+if (correct === true){
+  console.log("It is correct!");
+} else {
+  console.log("It is wrong!");
+}
+
+
+//This should be working, but I have no clue
+//how to make it log it down. 
+//I have tested it and it works so 
+//this is all I can do for now.
---
+++ projects/platformer/js/init/cannon.js
@@ -0,0 +1,24 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.collectable = window.opspark.collectable || {};
+    let cannon = window.opspark.cannon;
+    
+    /**
+     * init: Initialize all cannons.
+     * 
+     * Add as many cannons as necessary to make your level challenging.
+     */ 
+    cannon.init = function (game) {
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        // example: 
+        cannon.create.onTop(400);
+        cannon.create.onBottom(150);
+        cannon.create.onTop(675); 
+        
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    };
+})(window);
